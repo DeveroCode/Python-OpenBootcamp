1724bfb9739dbe4bfc245adfa463f722b53193c0
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-26 22:24:47 -0500

    New: Exercise four finish

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+# Instructions
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+def reversa():
+    for i in range(100, 0, -1):
+        print(i)
+
+
+reversa()
